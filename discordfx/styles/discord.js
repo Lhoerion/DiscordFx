f2f6a8464cf596e405b9eeabf26371d9465cddf3
Martin <7252614+Lhoerion@users.noreply.github.com>
2021-04-04 00:21:30 +0200

    Rework affix

--- discordfx/styles/discord.js
+++ discordfx/styles/discord.js
@@ -1,12 +1,108 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 $(function() {
+  renderAffix();
   $('a:not([data-tab])').off("click").on("click", delegateAnchors);
   $(".blackout").on("click", toggleMenu);
   $(".navbar-toggler").on("click", toggleMenu);
 });
 
-window.refresh = function (article) {}
+window.refresh = function(article) {
+  renderAffix();
+}
+
+function renderAffix() {
+  let tree = traverseArticle();
+  const el = $("#affix");
+  if (!el) return;
+  if (!tree || tree.length <= 0) {
+    el.hide();
+  } else {
+    el.show();
+    el.html(formList(tree, ["nav", "bs-docs-sidenav"]));
+  }
+
+  function traverseArticle() {
+    const isConceptual = $(".content-column").hasClass("Conceptual");
+    let headers = $(["h1", "h2", "h3", "h4"].map(el => "article.content " + el).join(", "));
+    let stack = [];
+    let curr = {};
+    headers.each(function () {
+      const el = $(this);
+      const xref = el.children().length > 1 ? el.children().last() : null;
+      const obj = {
+        parent: curr,
+        type: el.prop("tagName"),
+        id: el.prop("id"),
+        name: htmlEncode(el.text()),
+        href: xref?.hasClass("xref") ? xref.prop("href") : "#" + el.prop("id"),
+        children: []
+      };
+      if (!stack.length) {
+        stack.push(curr = obj);
+        return;
+      }
+      if (obj.type === stack[stack.length - 1].type) {
+        stack.push(curr = obj);
+      } else if (obj.type > curr.type) {
+        curr.children.push(curr = obj);
+      } else if (obj.type < curr.type) {
+        obj.parent = curr.parent.parent;
+        curr.parent.parent.children.push(curr = obj);
+      } else {
+        curr.parent.children.push(obj);
+      }
+    });
+
+    return stack.length && !isConceptual ? stack[0].children : stack;
+  }
+
+  function formList(item, classes) {
+    var level = 1;
+    return getList({ children: item }, [].concat(classes).join(" "));
+
+    function getList(model, cls) {
+      if (!model || !model.children) return null;
+      let l = model.children.length;
+      if (l === 0) return null;
+      let html = '<ul class="' + ['level' + level++, cls, model.id].filter(el => el != null).join(' ') + '">';
+      for (let i = 0; i < l; i++) {
+        let item = model.children[i];
+        let href = item.href;
+        let name = item.name;
+        if (!name) continue;
+        html += href ? '<li><a href="' + href + '">' + name.trim() + '</a>' : '<li>' + name;
+        let lvl = level;
+        html += getList(item, cls) || '';
+        level = lvl;
+        html += '</li>';
+      }
+      level = 1;
+      html += '</ul>';
+      return html;
+    }
+  }
+}
+
+function htmlEncode(str) {
+  if (!str) return str;
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+}
+
+function htmlDecode(value) {
+  if (!str) return str;
+  return value
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'")
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&amp;/g, '&');
+}
 
 function toggleMenu() {
   const el = !this.classList.contains("blackout") ? $(this) : null;
